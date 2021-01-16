779f2e62a620d30e5e55346453659664b28bb5c6
Nemanja Ćosović <ncosovic@gmail.com>
2021-01-16 13:56:59 +0100

    Logo
    Introduce SVG logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,22 @@ function App() {
   return (
     <div className="App">
       <div className="dkreg-wrapper">
-          <header>Header</header>
-          <div>Form</div>
-          <footer>Footer</footer>
+          <header>
+              <div>
+                  <h1>
+                      <a href="/" title="Hjemmeside">
+                          <img alt="DKreg logo" title="DKreg Hjemmeside" src="assets/dkreg-logo.svg"/>
+                          <span>DK reg</span>
+                      </a>
+                  </h1>
+              </div>
+          </header>
+          <main>
+              <div></div>
+          </main>
+          <footer>
+              <div></div>
+          </footer>
       </div>
     </div>
   );
